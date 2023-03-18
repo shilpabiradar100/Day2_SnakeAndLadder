f2e0123aed7bf51409468ee3f2ec55b45ad0a87e
shilpa biradar <108936323+shilpabiradar100@users.noreply.github.com>
2023-03-18 12:30:16 +0530

    Delete src/exception directory

--- src/exception/ExceptionDemo.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class ExceptionDemo {
-    public static void main(String[] args) {
-        String name = null; //
-        System.out.println(name.toLowerCase());
-        System.out.println("hi");
-    }
-}
